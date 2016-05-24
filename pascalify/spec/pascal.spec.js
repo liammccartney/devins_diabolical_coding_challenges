01777717b28bdc98475134a7cb335a7b312b3578
Liam McCartney <liam.mccartney@gmail.com>
2016-05-24 14:08:11 -0400

    Add more comprehensive test for pascalify

--- pascalify/spec/pascal.spec.js
+++ pascalify/spec/pascal.spec.js
@@ -14,4 +14,14 @@ describe('pascalify', function(){
     it('should work for 5', function(){
         expect(pascalify(5)).toEqual([[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]);
     });
+
+    it('should work for any number', function(){
+        var randNumber = Math.floor((Math.random() * 20) + 1);
+        var triangle = pascalify(randNumber);
+        var randIndex = Math.floor((Math.random() * triangle.length) + 1);
+        var randLevel = triangle[randIndex];
+        var levelSum = randLevel
+            .reduce(function(prevVal, currVal) { return prevVal + currVal});
+        expect(levelSum).toEqual(Math.pow(2, randIndex));
+    });
 });
